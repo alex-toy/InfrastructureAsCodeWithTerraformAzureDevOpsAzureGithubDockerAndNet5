e4cf9025e989a8ab7548e4fc6ec2262ea8898e93
alex-toy <alexei.80@hotmail.fr>
2022-12-29 13:45:45 +0100

    create SampleAPI project

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Infrastructure As Code With Terraform Azure DevOps Azure Github Docker And Net5
 
-https://www.youtube.com/watch?v=q4xNBqvD1uU
+In this projectn, we will be going through IaC with terraform and Azure DevOps. 
+
+
+## Create project
+
+<img src="/pictures/create_project.png" title="create project"  width="800">
+
+
+
+
+
+### Package Manager Command in **AzureBlobStorage**
+```
+Install-Package Azure.Storage.Blobs
+```
+
+### Create Storage Account
+
+<img src="/pictures/containers.png" title="containers"  width="800">
+<img src="/pictures/connection_strings.png" title="connection strings"  width="600">
---
+++ SampleAPI/SampleAPI/Controllers/PersonsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = new string[] { "alex", "seb" };
+            return Ok(persons);
+        }
+
+
+
+
+
+        // GET api/<PersonsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
